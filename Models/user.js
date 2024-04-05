d36b7a63722d6890eab771f70c141b57f1c1e76f
ajaymahiwal <ajaymahiwal5@gmail.com>
2024-04-05 21:48:22 +0530

    Connect

---
+++ Models/user.js
@@ -0,0 +1,69 @@
+
+
+const mongoose = require("mongoose");
+// const Schema = mongoose.Schema;
+const passportLocalMongoose = require('passport-local-mongoose');
+
+
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type:String,
+        required:true,
+    },
+    username:{
+        type:String,
+    },
+    googleId:{
+        type:String,
+    },
+    name:{
+        type:String,
+    },
+    admin:{
+        type:Boolean,
+    },
+    dob:{
+        type:Date,
+    },
+    gender:{
+        type:String,
+    },
+    contact_num:{
+        type:String,
+    },
+    bio:{
+        type:String,
+    },
+    image: {
+        type: String,
+        default:
+            "https://img.freepik.com/free-vector/cute-happy-smiling-child-isolated-white_1308-32243.jpg",
+        set: (v) =>
+            v === ""
+                ? "https://img.freepik.com/free-vector/cute-happy-smiling-child-isolated-white_1308-32243.jpg"
+                : v,
+    },
+    createdAt:{
+        type: Date,
+        default: Date.now(),
+    },
+    followers:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+    }],
+    followings:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+    }],
+    userSocailNetwork:[
+        {
+            appName:String,
+            profileURL:String,
+        }
+    ]
+});
+
+userSchema.plugin(passportLocalMongoose);
+
+module.exports = new mongoose.model("human",userSchema);
